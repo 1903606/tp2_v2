066391c8ec9149ef4b64c33ccff8500eaeca0cfa
1903606 <1903606@cmontmorency.qc.ca>
2023-12-05 17:40:19 -0500

    Fait: Activity main goes to activity register and register user can be done properly

--- app/src/main/java/com/example/travailpratique2_v2/MainActivity.java
+++ app/src/main/java/com/example/travailpratique2_v2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.travailpratique2_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_register = findViewById(R.id.btn_inscription);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_register_view = new Intent(MainActivity.this, Register.class );
+                startActivity(intent_register_view);
+                finish();
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/travailpratique2_v2/Register.java
+++ app/src/main/java/com/example/travailpratique2_v2/Register.java
@@ -1,14 +1,109 @@
 package com.example.travailpratique2_v2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
+    TextInputEditText tiet_courriel, tiet_mdp, tiet_mdp_confirmez;
+    Button btn_register;
+    FirebaseAuth bdAuth;
+    Dialog bteDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tiet_courriel = findViewById(R.id.tiet_courriel);
+        tiet_mdp = findViewById(R.id.tiet_mdp);
+        tiet_mdp_confirmez = findViewById(R.id.tiet_mdp_confirmez);
+        btn_register = findViewById(R.id.btn_register);
+
+
+
+        bdAuth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String courriel = tiet_courriel.getText().toString();
+                String mdp = tiet_mdp.getText().toString();
+                String mdp_confirmez = tiet_mdp_confirmez.getText().toString();
+
+                if (TextUtils.isEmpty(courriel)){
+                    Toast.makeText(Register.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(mdp)){
+                    Toast.makeText(Register.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(mdp_confirmez)){
+                    Toast.makeText(Register.this, "Please validate your password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!mdp.equals(mdp_confirmez)){
+                    Toast.makeText(Register.this, "Please enter same password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(mdp.length() < 10){
+                    Toast.makeText(Register.this, "Please minimum 10 caracters", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(mdp_confirmez.length() < 10){
+                    Toast.makeText(Register.this, "Please minimum 10 caracters", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                if(Patterns.EMAIL_ADDRESS.matcher(courriel).matches()){
+                    if (mdp.matches(mdp_confirmez)&& mdp.length()>= 10){
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+                        bdAuth.createUserWithEmailAndPassword(courriel, mdp)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(Register.this, "Utilisateur success", Toast.LENGTH_SHORT).show();
+                                            FirebaseUser usager = bdAuth.getCurrentUser();
+                                            if(usager!=null){
+                                                Intent intent_login_view = new Intent(Register.this, MainActivity.class );
+                                                startActivity(intent_login_view);
+                                                finish();
+                                            }
+                                        } else {
+                                            Toast.makeText(Register.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                        }
+
+                }
+
+            }
+        });
     }
 }
